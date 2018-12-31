785875725635dc04b055ff61c2fee222b76a7865
Zach <zrb@umd.edu>
2018-12-31 17:18:55 -0500

    removed log class, added TODO to main

--- balloontelemetry/main.py
+++ balloontelemetry/main.py
@@ -12,3 +12,11 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.par
 if __name__ == '__main__':
     json_filename = sys.argv[1]
     kml_filename = sys.argv[2] if len(sys.argv) > 2 else None
+
+    ground_tracks = {}
+
+    # TODO add main code here. Workflow:
+    #  1) APRS packet comes in over serial
+    #  2) parse raw packet from serial
+    #  3) add to respective ground track
+    #  4) display and log packet
