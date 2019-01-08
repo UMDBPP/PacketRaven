8ae295b6e6363dfcc8ee4cd8e1c8dd7cf831b064
Zach <zrb@umd.edu>
2019-01-08 16:33:38 -0500

    lon/lat decoding

--- balloontelemetry/telemetry/parsing.py
+++ balloontelemetry/telemetry/parsing.py
@@ -7,9 +7,9 @@ __authors__ = ['Zachary Burnett']
 
 def parse_aprs_packet(raw_string: str) -> dict:
     """
-    Parses APRS fields from packet
+    Parse APRS fields from raw packet string.
 
-    APRS packet reference: http://www.aprs.org/doc/APRS101.PDF
+    APRS format reference: http://www.aprs.org/doc/APRS101.PDF
 
     TODO detect partial packets
 
@@ -17,9 +17,28 @@ def parse_aprs_packet(raw_string: str) -> dict:
     :return: dictionary of APRS fields
     """
 
-    raw_string.split()
+    parsed_packet = {}
 
-    return {}
+    return parsed_packet
+
+
+def decode_lonlat(encoded_lonlat: str) -> float:
+    converted_floats = []
+    current_power = len(encoded_lonlat) - 1
+
+    for character in encoded_lonlat:
+        converted_floats.append(float(ord(character) - 33) * (91 ** current_power))
+        current_power -= 1
+
+    return sum(converted_floats)
+
+
+def convert_lon(compressed_lon: str) -> float:
+    return -180 + (decode_lonlat(compressed_lon) / 190463)
+
+
+def convert_lat(compressed_lat: str) -> float:
+    return 90 - (decode_lonlat(compressed_lat) / 380926)
 
 
 if __name__ == '__main__':
