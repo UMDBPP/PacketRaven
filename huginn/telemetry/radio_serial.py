b375edd3c1c6149e0c927a56614e4cbf643ae36e
Zach <zrb@umd.edu>
2019-02-02 17:32:19 -0500

    changed readlines to readline

--- huginn/telemetry/radio_serial.py
+++ huginn/telemetry/radio_serial.py
@@ -9,23 +9,26 @@ import serial
 APRS_PACKET_MINIMUM_LENGTH = 12
 
 
-def raw_packets_from_serial(port_name: str, timeout: int = 1) -> list:
+def raw_packets_from_serial(port_name: str, max_lines: int = 10) -> list:
     """
     Get potential packet strings from given serial port.
 
     :param port_name: serial port to radio
-    :param baud_rate: baud rate of serial connection
-    :param timeout: seconds to wait for serial output
+    :param max_lines: maximum number of lines to extract
     :return: list of candidate packet strings
     """
 
     packet_candidates = []
 
-    with serial.Serial(port_name, timeout=timeout) as serial_connection:
+    with serial.Serial(port_name) as serial_connection:
         # check if serial connection has data in its input buffer
         if serial_connection.in_waiting > 0:
-            serial_output = serial_connection.readlines()
-            for line in serial_output:
+            num_lines = 0
+
+            while num_lines <= max_lines:
+                line = serial_connection.readline()
+                num_lines += 1
+
                 if len(line) > APRS_PACKET_MINIMUM_LENGTH:
                     packet_candidates.append(line.decode('utf-8'))
 
