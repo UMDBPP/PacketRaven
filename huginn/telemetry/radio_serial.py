ce7b7a2d816dce690258bc9dba920e91b6000367
Zach <zrb@umd.edu>
2019-02-02 17:38:31 -0500

    fixed serial readlines (removved in_waiting)

--- huginn/telemetry/radio_serial.py
+++ huginn/telemetry/radio_serial.py
@@ -9,34 +9,30 @@ import serial
 APRS_PACKET_MINIMUM_LENGTH = 12
 
 
-def raw_packets_from_serial(port_name: str, max_lines: int = 10) -> list:
+def raw_packets_from_serial(port_name: str, timeout: int = 1) -> list:
     """
     Get potential packet strings from given serial port.
 
     :param port_name: serial port to radio
-    :param max_lines: maximum number of lines to extract
+    :param timeout: timeout to wait for radio lines
     :return: list of candidate packet strings
     """
 
     packet_candidates = []
 
-    with serial.Serial(port_name) as serial_connection:
+    with serial.Serial(port_name, timeout=1) as serial_connection:
         # check if serial connection has data in its input buffer
-        if serial_connection.in_waiting > 0:
-            num_lines = 0
-
-            while num_lines <= max_lines:
-                line = serial_connection.readline()
-                num_lines += 1
-
-                if len(line) > APRS_PACKET_MINIMUM_LENGTH:
-                    packet_candidates.append(line.decode('utf-8'))
+        lines = serial_connection.readlines()
+        for line in lines:
+            if len(line) > APRS_PACKET_MINIMUM_LENGTH:
+                packet_candidates.append(line.decode('utf-8'))
 
     return packet_candidates
 
 
 if __name__ == '__main__':
-    packet_candidates = raw_packets_from_serial('/dev/ttyUSB0')
+    # packet_candidates = raw_packets_from_serial('/dev/ttyUSB0')
+    packet_candidates = raw_packets_from_serial('COM5')
 
     print(f'{len(packet_candidates)} packet candidates found')
     print(packet_candidates)
