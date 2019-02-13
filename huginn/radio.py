64e4e782b4eda4b07a9881d6f09901801b30c8fa
Zach <zrb@umd.edu>
2019-02-12 21:39:11 -0500

    basic gui

--- huginn/radio.py
+++ huginn/radio.py
@@ -8,6 +8,15 @@ import serial
 from serial.tools import list_ports
 
 
+def find_port() -> str:
+    com_ports = serial.tools.list_ports.comports()
+
+    if len(com_ports) > 0:
+        return com_ports[0].device
+    else:
+        return None
+
+
 def connect(serial_port: str = None) -> serial.Serial:
     """
     Connect to radio over serial port.
@@ -17,17 +26,15 @@ def connect(serial_port: str = None) -> serial.Serial:
     """
 
     if serial_port is None:
-        com_ports = serial.tools.list_ports.comports()
+        serial_port = find_port()
 
-        if len(com_ports) == 0:
-            raise EnvironmentError('No open serial ports found.')
-
-        serial_port = com_ports[0].device
+    if serial_port is None:
+        raise EnvironmentError('No open serial ports found.')
 
     return serial.Serial(serial_port, baudrate=9600, timeout=1)
 
 
-def packets(serial_connection: serial.Serial) -> list:
+def get_packets(serial_connection: serial.Serial) -> list:
     """
     Get potential packet strings from given serial connection.
 
@@ -39,5 +46,5 @@ def packets(serial_connection: serial.Serial) -> list:
 
 
 if __name__ == '__main__':
-    packet_candidates = packets(connect())
+    packet_candidates = get_packets(connect())
     print(packet_candidates)
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ __authors__ = ['Quinn Kupec', 'Zachary Burnett']
 
 import datetime
 import os
-import re
 import sys
 import time
 
@@ -18,36 +17,31 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.par
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        callsigns = sys.argv[1]
+        log_filename = sys.argv[1]
     else:
-        callsigns = ['W3EAX-13']
+        log_filename = os.path.join(os.path.expanduser('~'), 'Desktop', 'log')
 
     if len(sys.argv) > 2:
-        log_directory = sys.argv[2]
-    else:
-        log_directory = os.path.join(os.path.expanduser('~'), 'Desktop', 'log')
-
-    if len(sys.argv) > 3:
-        serial_port = sys.argv[3]
+        serial_port = sys.argv[2]
     else:
         serial_port = None
 
-    if not os.path.exists(log_directory):
-        os.mkdir(log_directory)
-
-    log_filename = os.path.join(log_directory, f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")}_huginn_log.txt')
-
-    logbook.FileHandler(log_filename, level='DEBUG', bubble=True).push_application()
-    logbook.StreamHandler(sys.stdout, level='INFO', bubble=True).push_application()
-    logger = logbook.Logger('Huginn')
-
-    ground_tracks = {}
+    if os.path.isdir(log_filename):
+        if not os.path.exists(log_filename):
+            os.mkdir(log_filename)
+        log_filename = os.path.join(log_filename, f'{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")}_huginn_log.txt')
 
     with radio.connect(serial_port) as radio_connection:
-        logger.info(f'Serial connection established on {radio_connection.name}.')
+        logbook.FileHandler(log_filename, level='DEBUG', bubble=True).push_application()
+        logbook.StreamHandler(sys.stdout, level='INFO', bubble=True).push_application()
+        logger = logbook.Logger('Huginn')
+
+        packet_tracks = {}
+
+        logger.info(f'Opening {radio_connection.name}.')
 
         while True:
-            raw_packets = radio.packets(radio_connection)
+            raw_packets = radio.get_packets(radio_connection)
 
             for raw_packet in raw_packets:
                 try:
@@ -59,17 +53,14 @@ if __name__ == '__main__':
                 if parsed_packet is not None:
                     callsign = parsed_packet['callsign']
 
-                    for callsign in callsigns:
-                        callsign_regex = re.compile(callsign)
-
-                    if callsign in ground_tracks:
-                        ground_tracks[callsign].append(parsed_packet)
+                    if callsign in packet_tracks:
+                        packet_tracks[callsign].append(parsed_packet)
                     else:
-                        ground_tracks[callsign] = tracks.APRSTrack(callsign, [parsed_packet])
+                        packet_tracks[callsign] = tracks.APRSTrack(callsign, [parsed_packet])
 
-                    ascent_rate = ground_tracks[callsign].ascent_rate()
-                    ground_speed = ground_tracks[callsign].ground_speed()
-                    seconds_to_impact = ground_tracks[callsign].seconds_to_impact()
+                    ascent_rate = packet_tracks[callsign].ascent_rate()
+                    ground_speed = packet_tracks[callsign].ground_speed()
+                    seconds_to_impact = packet_tracks[callsign].seconds_to_impact()
 
                     logger.info(
                         f'{parsed_packet} ascent_rate={ascent_rate} ground_speed={ground_speed} seconds_to_impact={seconds_to_impact}')
