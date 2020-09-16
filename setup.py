a5c80c880f4ff4ad409168c6764512ee6117ed30
Zach <zrb@umd.edu>
2020-09-16 17:49:08 -0400

    lower version restriction

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     url=metadata['url'],
     packages=find_packages(),
-    python_requires='>=3.8',
+    python_requires='>=3.6',
     setup_requires=['dunamai', 'setuptools>=41.2'],
     install_requires=[
         'dunamai',
