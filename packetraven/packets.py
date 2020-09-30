9859c01fef6345d8d75105cc05719a915ea2f8a2
Zach <zrb@umd.edu>
2020-09-30 15:06:06 -0400

    fix database packet attributes

--- packetraven/packets.py
+++ packetraven/packets.py
@@ -1,14 +1,11 @@
 from datetime import datetime, timedelta
 import math
-from typing import Any, TYPE_CHECKING, Union
+from typing import Any, Union
 
 import numpy
 from pyproj import CRS, Geod, Transformer
 
 from .parsing import parse_raw_aprs
-
-if TYPE_CHECKING:
-    from .connections import PacketConnection
 
 DEFAULT_CRS = CRS.from_epsg(4326)
 
@@ -16,7 +13,7 @@ DEFAULT_CRS = CRS.from_epsg(4326)
 class LocationPacket:
     """ location packet encoding (x, y, z) and time """
 
-    def __init__(self, time: datetime, x: float, y: float, z: float = None, crs: CRS = None, source: 'PacketConnection' = None,
+    def __init__(self, time: datetime, x: float, y: float, z: float = None, crs: CRS = None, source: str = None,
                  **kwargs):
         self.time = time
         self.coordinates = numpy.array((x, y, z if z is not None else 0))
