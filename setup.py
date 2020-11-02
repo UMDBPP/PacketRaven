15b5a54e463ea97751b44d644ac56c55d4f87ec6
Zach <zrb@umd.edu>
2020-11-02 15:01:42 -0500

    automatically install `dunamai` from within `setup.py` (#23)
    * fix igate
    
    * disable igate for now
    
    * create packet frame
    
    * update name
    
    * don't test aprs is connection
    
    * uncomment igate parameter
    
    * fixes for aprs packet and igate send
    
    * fixes for aprs packet and igate send
    
    * fix tests and references to `from_callsign` and `to_callsign`
    
    * automatically install `dunamai` from within `setup.py`
    
    * update tablecrow references
    
    * tablecrow version
    
    * update pip
    
    * version restriction
    
    * version restriction
    
    * upgrade pip

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
-from dunamai import Version
 from setuptools import config, find_packages, setup
+
+try:
+    from dunamai import Version
+except ImportError:
+    import sys
+    import subprocess
+
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'dunamai'])
+    from dunamai import Version
 
 metadata = config.read_configuration('setup.cfg')['metadata']
 
@@ -14,8 +22,8 @@ setup(
     long_description_content_type="text/markdown",
     url=metadata['url'],
     packages=find_packages(),
-    python_requires='>=3.6',
-    setup_requires=['dunamai', 'setuptools>=41.2', 'wheel'],
+    python_requires='>=3.8',
+    setup_requires=['dunamai', 'setuptools>=41.2'],
     install_requires=[
         'aprslib',
         'haversine',
@@ -28,9 +36,9 @@ setup(
         'psycopg2-binary',
         'shapely',
         'sshtunnel',
-        'tablecrow>=1.0.5',
+        'tablecrow',
     ],
-    extras_require={'dev': ['coverage', 'flake8', 'nose']},
+    extras_require={'testing': ['coverage', 'flake8', 'nose']},
     entry_points={'console_scripts': ['packetraven=client.cli:main']},
     test_suite='nose.collector'
 )
