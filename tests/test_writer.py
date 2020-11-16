7e18bfbdaec5b4791c1a1439b6f788d5984631f0
Zach <zrb@umd.edu>
2020-11-16 13:33:16 -0500

    update reference files with timezones

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
+import pytz
+
 from packetraven.packets import APRSPacket
 from packetraven.tracks import APRSTrack
 from packetraven.utilities import repository_root
@@ -14,17 +16,17 @@ PACKET_TRACK = APRSTrack(
     [
         APRSPacket.from_frame(
             "W3EAX-8>APRS,WIDE1-1,WIDE2-1,qAR,K3DO-11:!/:Gh=:j)#O   /A=026909|!Q|  /W3EAX,262,0,18'C,http://www.umd.edu",
-            packet_time=datetime(2018, 11, 11, 10, 20, 13),
+            packet_time=pytz.timezone("America/New_York").localize(datetime(2018, 11, 11, 10, 20, 13)),
         ),
         APRSPacket.from_frame(
             "W3EAX-8>APRS,N3TJJ-12,WIDE1*,WIDE2-1,qAR,N3FYI-2:!/:GiD:jcwO   /A=028365|!R|  /W3EAX,267,0,18'C,"
             'http://www.umd.edu',
-            packet_time=datetime(2018, 11, 11, 10, 21, 24),
+            packet_time=pytz.timezone("America/New_York").localize(datetime(2018, 11, 11, 10, 21, 24)),
         ),
         APRSPacket.from_frame(
             "W3EAX-13>APRS,KC3FIT-1,WIDE1*,WIDE2-1,qAR,KC3AWP-10:!/:JL2:u4wO   /A=043080|!j|  /W3EAX,326,0,20'C,"
             'nearspace.umd.edu',
-            packet_time=datetime(2019, 2, 3, 14, 39, 28),
+            packet_time=pytz.timezone("America/New_York").localize(datetime(2019, 2, 3, 14, 39, 28)),
         ),
     ],
 )
@@ -56,5 +58,5 @@ def test_write_txt():
     with TemporaryDirectory() as temporary_directory:
         output_filename = Path(temporary_directory) / filename
         write_aprs_packet_tracks([PACKET_TRACK], output_filename)
-        with  open(output_filename) as output_file, open(reference_filename) as reference_file:
+        with open(output_filename) as output_file, open(reference_filename) as reference_file:
             assert output_file.read() == reference_file.read()
