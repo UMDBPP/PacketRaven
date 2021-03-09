1624fad820d807a3c4d6d4167bc40835ad7ac24b
Zach <zrb@umd.edu>
2021-03-09 18:02:32 -0500

    add conda to setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,70 @@
 #!/usr/bin/env python
+import importlib
+import logging
+import os
+from pathlib import Path
+import subprocess
+import sys
+
 from setuptools import config, find_packages, setup
 
+BUILT_PACKAGES = {'numpy': [], 'pyproj': []}
+is_conda = (Path(sys.prefix) / 'conda-meta').exists()
+
+if is_conda:
+    conda_packages = []
+    for conda_package in BUILT_PACKAGES:
+        try:
+            importlib.import_module(conda_package)
+        except:
+            conda_packages.append(conda_package)
+    if len(conda_packages) > 0:
+        subprocess.check_call(['conda', 'install', '-y', *conda_packages])
+
+if os.name == 'nt':
+    for required_package, pipwin_dependencies in BUILT_PACKAGES.items():
+        try:
+            importlib.import_module(required_package)
+        except:
+            try:
+                import pipwin
+            except:
+                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pipwin'])
+
+            failed_pipwin_packages = []
+            for pipwin_package in pipwin_dependencies + [required_package]:
+                try:
+                    subprocess.check_call([sys.executable, '-m', 'pipwin', 'install', pipwin_package.lower()])
+                except subprocess.CalledProcessError:
+                    failed_pipwin_packages.append(pipwin_package)
+
+            if len(failed_pipwin_packages) > 0:
+                raise RuntimeError(
+                    f'failed to download or install non-conda Windows build(s) of {" and ".join(failed_pipwin_packages)}; you can either\n'
+                    '1) install within an Anaconda environment, or\n'
+                    f'2) `pip install <file>.whl`, with `<file>.whl` downloaded from {" and ".join("https://www.lfd.uci.edu/~gohlke/pythonlibs/#" + value.lower() for value in failed_pipwin_packages)} for your Python version'
+                )
+
 try:
-    from dunamai import Version
-except ImportError:
-    import sys
-    import subprocess
-
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'dunamai'])
-    from dunamai import Version
-
-try:
-    import pyproj
-except ImportError:
-    import platform
-
-    if platform.system() == 'Windows':
+    try:
+        from dunamai import Version
+    except ImportError:
         import subprocess
+        import sys
 
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pipwin'])
-        subprocess.check_call([sys.executable, '-m', 'pipwin', 'install', 'pyproj'])
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'dunamai'])
+        from dunamai import Version
+
+    version = Version.from_any_vcs().serialize()
+except RuntimeError as error:
+    logging.exception(error)
+    version = '0.0.0'
 
 metadata = config.read_configuration('setup.cfg')['metadata']
 
 setup(
     name=metadata['name'],
-    version=Version.from_any_vcs().serialize(),
+    version=version,
     author=metadata['author'],
     author_email=metadata['author_email'],
     description=metadata['description'],
