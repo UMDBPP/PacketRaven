fb21b902d192cfde4829770a7a7435027a80edfd
Zach <zrb@umd.edu>
2021-04-02 17:26:04 -0400

    add TODO

--- packetraven/model.py
+++ packetraven/model.py
@@ -2,6 +2,7 @@ import numpy
 
 # `dh/dt` based on historical flight data
 FREEFALL_DESCENT_RATE = lambda altitude: -5.8e-08 * altitude ** 2 - 6.001
+# TODO: propagate uncertainty
 FREEFALL_DESCENT_RATE_UNCERTAINTY = lambda altitude: 0.2 * FREEFALL_DESCENT_RATE(altitude)
 
 # integration of `(1/(dh/dt)) dh` based on historical flight data
